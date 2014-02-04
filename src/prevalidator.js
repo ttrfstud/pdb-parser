790516650ba9888961d6604223f60351290972db
ttrfstud <ttrfstud@gmail.com>
2014-02-04 21:32:04 +0300

    in progress

--- src/prevalidator.js
+++ src/prevalidator.js
@@ -60,27 +60,62 @@ VALID_CHARACTERS[62] = 1;
 VALID_CHARACTERS[63] = 1;
 
 var VALID_RECORD_NAMES = {
-	'CRYST1' : 1,
-	'END   ' : 1,
-	'HEADER' : 1,
-	'NUMMDL' : 1,
-	'MASTER' : 1,
-	'ORIGX1' : 1,
-	'ORIGX2' : 1,
-	'ORIGX3' : 1,
-	'SCALE1' : 1,
-	'SCALE2' : 1,
-	'SCALE3' : 1,
-
-
+	'CRYST1 ' : 1,
+	'END    ' : 1,
+	'HEADER ' : 1,
+	'NUMMDL ' : 1,
+	'MASTER ' : 1,
+	'ORIGX1 ' : 1,
+	'ORIGX2 ' : 1,
+	'ORIGX3 ' : 1,
+	'SCALE1 ' : 1,
+	'SCALE2 ' : 1,
+	'SCALE3 ' : 1,
+	'AUTHOR ' : 1,
+	'CAVEAT ' : 1,
+	'COMPND ' : 1,
+	'EXPDTA ' : 1,
+	'MDLTYP ' : 1,
+	'KEYWDS ' : 1,
+	'OBSLTE ' : 1,
+	'SOURCE ' : 1,
+	'SPLIT  ' : 1,
+	'SPRSDE ' : 1,
+	'ANISOU ' : 1,
+	'ATOM   ' : 1,
+	'CISPEP ' : 1,
+	'CONECT ' : 1,
+	'DBREF  ' : 1,
+	'HELIX  ' : 1,
+	'HET    ' : 1,
+	'HETATM ' : 1,
+	'LINK   ' : 1,
+	'MODRES ' : 1,
+	'MTRIX1 ' : 1,
+	'MTRIX2 ' : 1,
+	'MTRIX3 ' : 1,
+	'REVDAT ' : 1,
+	'SEQADV ' : 1,
+	'SHEET  ' : 1,
+	'SSBOND ' : 1,
+	'FORMUL ' : 1,
+	'HETNAM ' : 1,
+	'HETSYN ' : 1,
+	'SEQRES ' : 1,
+	'SITE   ' : 1,
+	'ENDMDL ' : 1,
+	'MODEL  ' : 1,
+	'TER    ' : 1,
+	'JRNL   ' : 1,
+	'REMARK ' : 1
 };
-
 
 var PREVALIDATION_ERROR = 'PREVALIDATION_ERROR: ';
 
 exports.prevalidate = function (pdb) {
 	validateCharacters(pdb);
 	validateLineLength(pdb);
+	validateRecordTypes(pdb);
 }
 
 function validateCharacters(text) {
@@ -97,14 +132,113 @@ function validateLineLength(text) {
 		throw new Error(PREVALIDATION_ERROR + 'LINE IS NOT 80-LONG');
 	}
 
-	var split = text.split(/[\r\n]/g);
-
-	split = split.slice(0, split.length - 1);
+	var split = splitIntoLines(text);
 
 	for (var i = 0; i < split.length; i++) {
 		if (split[i].length !== 79) {
-			console.log('split', split[i].length);
 			throw new Error(PREVALIDATION_ERROR + 'LINE IS NOT 80-LONG');
 		}
 	}
 }
+
+function validateRecordTypes(text) {
+	var split = splitIntoLines(text);
+
+	for (var i = 0; i < split.length; i++) {
+		if (!VALID_RECORD_NAMES[split[i].substring(0, 7)]) {
+			throw new Error(PREVALIDATION_ERROR + 'INVALID RECORD TYPE!');
+		}
+	}
+}
+
+function validateRecordArity(text) {
+	var split = splitIntoLines(text);
+
+	var singletons = {
+		'CRYST1 ' : 0,
+		'END    ' : 0,
+		'HEADER ' : 0,
+		'NUMMDL ' : 0,
+		'MASTER ' : 0,
+		'ORIGX1 ' : 0,
+		'ORIGX2 ' : 0,
+		'ORIGX3 ' : 0,
+		'SCALE1 ' : 0,
+		'SCALE2 ' : 0,
+		'SCALE3 ' : 0
+	};
+
+	for (var i = 0; i < split.length; i++) {
+		singletons[split[i].substring(0, 7)]++; 
+	}
+
+	for (var i in singletons) if (singletons.hasOwnProperty(i)) {
+		if (singletons[i] > 1) {
+			throw new Error(PREVALIDATION_ERROR + 'INVALID SINGLETON RECORD ARITY!');
+		}
+	}
+}
+
+function validateMandatoryTypes(text) {
+	var mandatory = {
+		'HEADER ' : 0,
+		'TITLE  ' : 0,
+		'COMPND ' : 0,
+		'SOURCE ' : 0,
+		'KEYWDS ' : 0,
+		'EXPDTA ' : 0,
+		'AUTHOR ' : 0,
+		'REVDAT ' : 0,
+		'REMARK 2' : 0,
+		'REMARK 3' : 0,
+		'SEQRES ' : 0,
+		'CRYST1 ' : 0,
+		'ORIGX1 ' : 0,
+		'ORIGX2 ' : 0,
+		'ORIGX3 ' : 0,
+		'SCALE1 ' : 0,
+		'SCALE2 ' : 0,
+		'SCALE3 ' : 0,
+		'MASTER ' : 0,
+		'END    ' : 0,
+	};
+
+	var split = splitIntoLines(text);
+
+	for (var i = 0; i < split.length; i++) {
+		var firstApprox = split[i].substring(0, 7);
+
+		// console.log(firstApprox);
+		if (firstApprox === 'REMARK ') {
+			firstApprox = split[i].substring(0, 9);
+		}
+
+		mandatory[firstApprox]++;
+	}
+
+	for (var i in mandatory) if (mandatory.hasOwnProperty(i)) {
+		if (!mandatory[i]) {
+			// console.log(i);
+			throw new Error(PREVALIDATION_ERROR + 'NOT ALL MANDATORY TYPES ARE PRESENT!');
+		}
+	}
+}
+
+function validateOrder(text) {
+	
+}
+
+function splitIntoLines(pdb) {
+	var split = pdb.split(/[\r\n]/g);
+
+	split = split.slice(0, split.length - 1);
+
+	return split;
+}
+
+exports.validateCharacters = validateCharacters;
+exports.validateLineLength = validateLineLength;
+exports.validateRecordTypes = validateRecordTypes;
+exports.validateRecordArity = validateRecordArity;
+exports.validateMandatoryTypes = validateMandatoryTypes;
+exports.validateOrder = validateOrder;
--- test/prevalidator-spec.js
+++ test/prevalidator-spec.js
@@ -1,5 +1,5 @@
 var should = require('should');
-var pv = require('../src/prevalidator').prevalidate;
+var pv = require('../src/prevalidator');
 
 describe('prevalidator handles', function () {
 	function complementLine(len) {
@@ -10,56 +10,56 @@ describe('prevalidator handles', function () {
 
 		it ('whitespace', function () {
 			(function () {
-				pv(' ' + complementLine(78) + '\n');
+				pv.validateCharacters(' ');
 			}).should.not.throw();
 		});
 
 		it ('10 and 13 newlines', function () {
 			(function () {
-				pv(complementLine(79) + String.fromCharCode(10) + complementLine(79) + '\n');
-				pv(complementLine(79) + String.fromCharCode(13) + complementLine(79) + '\n');
+				pv.validateCharacters(String.fromCharCode(10));
+				pv.validateCharacters(String.fromCharCode(13));
 			}).should.not.throw();
 		});
 
 		it ('digits', function () {
 			(function () {
-				pv('0123456789' + complementLine(69) + '\n');
+				pv.validateCharacters('0123456789');
 			}).should.not.throw();
 		});
 
 		it ('lowercase letters', function () {
 			(function () {
-				pv('abcdefghijklmnopqrstuvwxyz' + complementLine(53) + '\n');
+				pv.validateCharacters('abcdefghijklmnopqrstuvwxyz');
 			}).should.not.throw();
 		});
 
 		it ('uppercase letters', function () {
 			(function () {
-				pv('ABCDEFGHIJKLMNOPQRSTUVWXYZ' + complementLine(53) + '\n');
+				pv.validateCharacters('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 			}).should.not.throw();
 		});
 
 		it ('special symbols', function () {
 			(function () {
-				pv('`-=[]\\;\',./~!@#$%^&*()_+{}|:"<>?' + complementLine(47) + '\n');
+				pv.validateCharacters('`-=[]\\;\',./~!@#$%^&*()_+{}|:"<>?');
 			}).should.not.throw();
 		});
 
 		it ('should not allow any symbols besides those', function () {
 			(function () {
-				pv(String.fromCharCode(7) + complementLine(78) + '\n');
+				pv.validateCharacters(String.fromCharCode(7));
 			}).should.throw('PREVALIDATION_ERROR: INVALID CHARACTER');
 			(function () {
-				pv(String.fromCharCode(15) + complementLine(78) + '\n');
+				pv.validateCharacters(String.fromCharCode(15));
 			}).should.throw('PREVALIDATION_ERROR: INVALID CHARACTER');
 			(function () {
-				pv(String.fromCharCode(127) + complementLine(78) + '\n');
+				pv.validateCharacters(String.fromCharCode(127));
 			}).should.throw('PREVALIDATION_ERROR: INVALID CHARACTER');
 		});
 
 		it ('should not allow any symbols besides those at any position', function () {
 			(function () {
-				pv('a1' + String.fromCharCode(7) + complementLine(76) + '\n');
+				pv.validateCharacters('a1' + String.fromCharCode(7));
 			}).should.throw('PREVALIDATION_ERROR: INVALID CHARACTER');
 		});
 	});
@@ -67,47 +67,173 @@ describe('prevalidator handles', function () {
 	describe('line length (= 80)', function () {
 		it ('shorter lines are not ok', function () {
 			(function () {
-				pv(' ');
+				pv.validateLineLength(' ');
 			}).should.throw('PREVALIDATION_ERROR: LINE IS NOT 80-LONG');
 			(function () {
-				pv(complementLine(78));
+				pv.validateLineLength(complementLine(78));
 			}).should.throw('PREVALIDATION_ERROR: LINE IS NOT 80-LONG');
 		});
 
 		it ('longer lines are not ok', function () {
 			(function () {
-				pv(complementLine(81));
+				pv.validateLineLength(complementLine(81));
 			}).should.throw('PREVALIDATION_ERROR: LINE IS NOT 80-LONG');
 		});
 
 		it ('shorter lines among normal lines', function () {
 			(function () {
-				pv(complementLine(80) + '\n \n' + complementLine(80));
+				pv.validateLineLength(complementLine(80) + '\n \n' + complementLine(80));
 			}).should.throw('PREVALIDATION_ERROR: LINE IS NOT 80-LONG');
 		});
 
 		it ('longer lines among normal lines', function () {
 			(function () {
-				pv(complementLine(79) + '\n' + complementLine(80) + '\n' + complementLine(79));
+				pv.validateLineLength(complementLine(79) + '\n' + complementLine(80) + '\n' + complementLine(79));
 			}).should.throw('PREVALIDATION_ERROR: LINE IS NOT 80-LONG');
 		});
 
 		it ('all normal lines', function () {
 			(function () {
-				pv(complementLine(79) + '\n' + complementLine(79) + '\n');
+				pv.validateLineLength(complementLine(79) + '\n' + complementLine(79) + '\n');
 			}).should.not.throw();
 		});
 
 		it ('lines not ending in trailing space', function () {
 			(function () {
-				pv(complementLine(79));
+				pv.validateLineLength(complementLine(79));
 			}).should.throw('PREVALIDATION_ERROR: LINE IS NOT 80-LONG');
 		});
 
 		it ('lines not ending in multiple space', function () {
 			(function () {
-				pv(complementLine(79 + '\r\n'));
+				pv.validateLineLength(complementLine(79 + '\r\n'));
 			}).should.throw('PREVALIDATION_ERROR: LINE IS NOT 80-LONG');
 		});
+	});
+
+	describe('each record type name', function () {
+		it ('should pass all valid record names', function () {
+			(function () {
+				pv.validateRecordTypes('END    \n');
+				pv.validateRecordTypes('HEADER \n');
+				pv.validateRecordTypes('NUMMDL \n');
+				pv.validateRecordTypes('MASTER \n');
+				pv.validateRecordTypes('ORIGX1 \n');
+				pv.validateRecordTypes('ORIGX2 \n');
+				pv.validateRecordTypes('ORIGX3 \n');
+				pv.validateRecordTypes('SCALE1 \n');
+				pv.validateRecordTypes('SCALE2 \n');
+				pv.validateRecordTypes('SCALE3 \n');
+				pv.validateRecordTypes('AUTHOR \n');
+				pv.validateRecordTypes('CAVEAT \n');
+				pv.validateRecordTypes('COMPND \n');
+				pv.validateRecordTypes('EXPDTA \n');
+				pv.validateRecordTypes('MDLTYP \n');
+				pv.validateRecordTypes('KEYWDS \n');
+				pv.validateRecordTypes('OBSLTE \n');
+				pv.validateRecordTypes('SOURCE \n');
+				pv.validateRecordTypes('SPLIT  \n');
+				pv.validateRecordTypes('SPRSDE \n');
+				pv.validateRecordTypes('ANISOU \n');
+				pv.validateRecordTypes('ATOM   \n');
+				pv.validateRecordTypes('CISPEP \n');
+				pv.validateRecordTypes('CONECT \n');
+				pv.validateRecordTypes('DBREF  \n');
+				pv.validateRecordTypes('HELIX  \n');
+				pv.validateRecordTypes('HET    \n');
+				pv.validateRecordTypes('HETATM \n');
+				pv.validateRecordTypes('LINK   \n');
+				pv.validateRecordTypes('MODRES \n');
+				pv.validateRecordTypes('MTRIX1 \n');
+				pv.validateRecordTypes('MTRIX2 \n');
+				pv.validateRecordTypes('MTRIX3 \n');
+				pv.validateRecordTypes('REVDAT \n');
+				pv.validateRecordTypes('SEQADV \n');
+				pv.validateRecordTypes('SHEET  \n');
+				pv.validateRecordTypes('SSBOND \n');
+				pv.validateRecordTypes('FORMUL \n');
+				pv.validateRecordTypes('HETNAM \n');
+				pv.validateRecordTypes('HETSYN \n');
+				pv.validateRecordTypes('SEQRES \n');
+				pv.validateRecordTypes('SITE   \n');
+				pv.validateRecordTypes('ENDMDL \n');
+				pv.validateRecordTypes('MODEL  \n');
+				pv.validateRecordTypes('TER    \n');
+				pv.validateRecordTypes('JRNL   \n');
+				pv.validateRecordTypes('REMARK \n');
+			}).should.not.throw();
+		});
+		it ('should not pass record type names without subsequent blank', function () {
+			(function () {
+				pv.validateRecordTypes('CRYST1\n');
+			}).should.throw('PREVALIDATION_ERROR: INVALID RECORD TYPE!');
+		});
+
+		it ('multiple lines of correct record type names', function () {
+			(function () {
+				pv.validateRecordTypes('CRYST1 \nENDMDL \nENDMDL \n');
+			}).should.not.throw();
+		});
+
+		it ('multiple lines intercepted by incorrectly padded name', function () {
+			(function () {
+				pv.validateRecordTypes('CRYST1 \nENDMDL\nENDMDL \n');
+			}).should.throw('PREVALIDATION_ERROR: INVALID RECORD TYPE!');
+		});
+
+		it('absolutely incorrect name', function () {
+			(function () {
+				pv.validateRecordTypes('INCRT \n');
+			}).should.throw('PREVALIDATION_ERROR: INVALID RECORD TYPE!');
+		});
+	});
+
+	describe('singleton record arity', function () {
+		it('unhappy path', function () {
+			(function () {
+				pv.validateRecordArity('CRYST1 \nCRYST1 \n');
+			}).should.throw('PREVALIDATION_ERROR: INVALID SINGLETON RECORD ARITY!');
+		});
+	});
+
+	describe('immediately mandatory types', function () {
+		it('if the pdb has all immediately mandatory types', function () {
+			(function () {
+				pv.validateMandatoryTypes(
+					'HEADER \n' + 
+					'TITLE  \n' + 
+					'COMPND \n' + 
+					'SOURCE \n' + 
+					'KEYWDS \n' + 
+					'EXPDTA \n' + 
+					'AUTHOR \n' + 
+					'REVDAT \n' + 
+					'REMARK 2\n' + 
+					'REMARK 3\n' + 
+					'SEQRES \n' + 
+					'CRYST1 \n' + 
+					'ORIGX1 \n' + 
+					'ORIGX2 \n' + 
+					'ORIGX3 \n' + 
+					'SCALE1 \n' + 
+					'SCALE2 \n' + 
+					'SCALE3 \n' + 
+					'MASTER \n' + 
+					'END    \n');
+			}).should.not.throw();
+		});
+
+		it ('if some of them is missing, it is error', function () {
+			(function () {
+				pv.validateMandatoryTypes('HEADER \n' + 
+					'TITLE  \n' + 'COMPND \n' + 'SOURCE \n' + 'EXPDTA \n' + 'AUTHOR \n' + 'REVDAT \n' + 
+					'REMARK 2\n' + 'REMARK 3\n' + 'SEQRES \n ' + 'CRYST1 \n' + 'ORIGX1 \n' + 
+					'ORIGX2 \n' + 'ORIGX3 \n' + 'SCALE1 \n' + 'SCALE2 \n' + 'SCALE3\n' + 'MASTER \n' + 'END\n');
+			}).should.throw('PREVALIDATION_ERROR: NOT ALL MANDATORY TYPES ARE PRESENT!');
+		});
+	});
+
+	describe('order of record types', function () {
+
 	});
 });
